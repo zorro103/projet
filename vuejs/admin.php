4fa0a2accef57bae91fa1aac9b9dba6c546a2d97
zorro103 <chowee2@gmail.com>
2020-05-06 09:33:08 +0200

    Envoi Poo + Vue

--- vuejs/admin.php
+++ vuejs/admin.php
@@ -3,7 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vue.js - Exemple 02 : Afficher section au click</title>
+    <title>Document</title>
+    <style>
+section.active {
+    position:fixed;
+    top:0;
+    left:0;
+    z-index:999;
+    width:100%;
+    height:100%;
+    background-color: rgba(0,0,0,0.8);
+    padding:10vh;
+}        
+    </style>
 </head>
 <body>
     <!-- ETAPE2: DELIMITER LA ZONE D'ACTION DE VUEJS -->
@@ -11,14 +23,31 @@
         <header>
             <h1>MON BACK OFFICE ({{ page }})</h1>
             <nav>
-                <a href="#page1" @click="page ='a'">page1</a>
-                <a href="#page2" @click="page ='b'">page2</a>
-                <a href="#page3" @click="page ='c'">page3</a>
+                <a href="#page1" @click="page = 'a'">page1</a>
+                <a href="#page2" @click="page = 'b'">page2</a>
+                <a href="#page3" @click="page = 'c'">page3</a>
             </nav>
         </header>
         <main>
             <section v-if="page == 'a'">
                 <h2>CRUD SUR LES TODOS</h2>
+                <section v-show="todoUpdate" :class="{ active : todoUpdate }">
+                    <h3>UPDATE</h3>
+                    <form v-if="todoUpdate">
+                        <input type="text" name="titre" v-model="todoUpdate.titre">
+                        <!-- POUR FERMER LA POPUP, ON AGIT SUR LA PROPRIETE VUEJS todoUpdate -->
+                        <button @click="todoUpdate=null">fermer la popup</button>
+                    </form>
+                </section>
+                <section>
+                    <h3>READ</h3>
+                    <div class="listeArticle">
+                        <article v-for="todo in todos">
+                            <h3>{{ todo.titre }}</h3>
+                            <button @click="modifier(todo)">modifier</button>
+                        </article>
+                    </div>
+                </section>
             </section>
             <section v-if="page == 'b'">
                 <h2>CRUD SUR LES USERS</h2>
@@ -42,8 +71,23 @@
 var app = new Vue({
   el: '#app',
   data: {
+    todoUpdate: null,
+    todos: 
+        // JSON ;-p
+        [
+            { titre: 'Apprendre JavaScript' },
+            { titre: 'Apprendre Vue' },
+            { titre: 'Créer quelque chose de génial' }
+        ],
     page: 'a',  
     message: 'Hello Vue !'
+  },
+  methods: {
+      modifier: function(todo)
+      {
+        this.todoUpdate = todo;
+        console.log(this.todoUpdate);
+      }
   }
 })        
     </script>
