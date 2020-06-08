1e779ef547479d2e58ec49a6ff3dc501a6bbf025
zorro103 <chowee2@gmail.com>
2020-06-08 16:11:44 +0200

    Envoi skills-management_project 04

--- skills-management_project/php/view/footer.php
+++ skills-management_project/php/view/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
     </main>
 
@@ -9,16 +8,4 @@
 
 
 </body>
-=======
-
-    </main>
-
-    <footer>
-        Tous Droits réservés &copy; 2020
-    </footer>
-    <script src="assets/js/app.js"></script>
-
-
-</body>
->>>>>>> e3dbcfcf122cc0cdebc092107e8284ec9e1710d2
 </html>
--- skills-management_project/php/view/section-logout.php
+++ skills-management_project/php/view/section-logout.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
         <section>
             <script>
@@ -7,14 +6,4 @@
             // REDIRECTION VERS LA PAGE D'ACCUEIL
             location.href = 'index.php';
             </script>
-=======
-
-        <section>
-            <script>
-            // ON VA SIMPLEMENT EFFACER LA CLE API DANS SESSION STORAGE
-            sessionStorage.removeItem('cleApi');
-            // REDIRECTION VERS LA PAGE D'ACCUEIL
-            location.href = 'index.php';
-            </script>
->>>>>>> e3dbcfcf122cc0cdebc092107e8284ec9e1710d2
         </section>
